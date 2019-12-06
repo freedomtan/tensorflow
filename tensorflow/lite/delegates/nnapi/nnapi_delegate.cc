b4c55f54d35626cc7e4a00cdc772e80310f76156
Koan-Sin Tan <koansin.tan@gmail.com>
2019-12-06 09:27:56 +0800

    hack: skip the avg pooling h*w <=256 check

--- tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
@@ -4000,6 +4000,7 @@ TfLiteStatus StatefulNnApiDelegate::DoPrepare(TfLiteContext* context,
       if (device_count <= 1) {
         return kTfLiteOk;
       }
+      is_accelerator_specified = true;
     }
   }
   // Allocate one element in vector already since TensorFlow Lite uses
