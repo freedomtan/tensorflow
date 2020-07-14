a4fba172e412d78781537c87c43e2664721da46a
Koan-Sin Tan <koansin.tan@gmail.com>
2020-07-14 23:16:35 +0800

    ignore class=0 and calclulate miou of all the images

--- tensorflow/lite/examples/deeplab/deeplabv3_ade20k_31.cc
+++ tensorflow/lite/examples/deeplab/deeplabv3_ade20k_31.cc
@@ -15,14 +15,14 @@ limitations under the License.
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
+#include "bitmap_helpers.h"
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
 #include "tensorflow/lite/optional_debug_tools.h"
-
-#include "bitmap_helpers.h"
 
 using namespace tflite;
 using namespace tflite::deeplab;
@@ -33,17 +33,11 @@ using namespace tflite::deeplab;
     exit(1);                                                 \
   }
 
-int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    fprintf(stderr, "minimal <tflite model> <bitmap file>\n");
-    return 1;
-  }
-
-  auto filename = argv[1];
-  auto ground_truth_file = argv[2];
-  auto input_bitmap_file = argv[3];
-
-  int w, h, d;
+void run_model(std::unique_ptr<Interpreter> &interpreter,
+               string ground_truth_file, string input_bitmap_file,
+               std::vector<uint64_t> &true_positives,
+               std::vector<uint64_t> &false_positives,
+               std::vector<uint64_t> &false_negatives) {
   std::ifstream stream(ground_truth_file, std::ios::in | std::ios::binary);
   std::vector<uint8_t> ground_truth_vector(
       (std::istreambuf_iterator<char>(stream)),
@@ -52,25 +46,11 @@ int main(int argc, char* argv[]) {
   std::cout << "len: " << ground_truth_vector.size() << "\n";
 #endif
 
+  int w, h, d;
   auto bitmap_vector = read_bmp(input_bitmap_file, w, h, d);
 #if __DEBUG__
   std::cout << w << ", " << h << ", " << d << "\n";
 #endif
-
-  // Load model
-  std::unique_ptr<tflite::FlatBufferModel> model =
-      tflite::FlatBufferModel::BuildFromFile(filename);
-  TFLITE_DEEPLAB_CHECK(model != nullptr);
-
-  // Build the interpreter
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-  InterpreterBuilder builder(*model, resolver);
-  std::unique_ptr<Interpreter> interpreter;
-  builder(&interpreter);
-  TFLITE_DEEPLAB_CHECK(interpreter != nullptr);
-
-  // Allocate tensor buffers.
-  TFLITE_DEEPLAB_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
 
   // Fill input buffers
   // assuming single input
@@ -111,13 +91,9 @@ int main(int argc, char* argv[]) {
   std::cout << "output_type: " << interpreter->tensor(output_tensor_index)->type
             << "\n";
 #endif
-
-  std::vector<uint64_t> tp_acc, fp_acc, fn_acc;
-
   for (int c = 1; c < 32; c++) {
-    uint64_t true_positive = 0;
-    uint64_t false_positive = 0;
-    uint64_t false_negative = 0;
+    uint64_t true_positive = 0, false_positive = 0, false_negative = 0;
+
     for (int i = 0; i < (512 * 512 - 1); i++) {
       auto p = (uint8_t)0x000000ff & output[i];
       auto g = ground_truth_vector[i];
@@ -125,7 +101,7 @@ int main(int argc, char* argv[]) {
       // 0xff means ignore
       // if ((g != 0xff) && (g != 0)) {
       if (g != 0xff) {
-	// trichotomy
+        // trichotomy
         if ((p == c) or (g == c)) {
           if (p == g)
             true_positive++;
@@ -137,23 +113,98 @@ int main(int argc, char* argv[]) {
       }
     }
 
-    tp_acc.push_back(true_positive);
-    fp_acc.push_back(false_positive);
-    fn_acc.push_back(false_negative);
+    true_positives.push_back(true_positive);
+    false_positives.push_back(false_positive);
+    false_negatives.push_back(false_negative);
   }
-
-  // miou = true_positive / (true_positive + false_positive + false_negative)
-#if 0
-  auto miou = tp_acc * 1.0 / (tp_acc + fp_acc + fn_acc);
-  std::cout << "mIOU: " << miou << "\n";
-#endif
-
+#if __DEBUG__
   for (int i = 0; i < 31; i++) {
-    std::cout << tp_acc[i] << ", " << fp_acc[i] << ", " << fn_acc[i];
-    if (i < 30)
-      std::cout << ", ";
+    std::cout << true_positives[i] << ", " << false_positives[i] << ", "
+              << false_negatives[i];
+    if (i < 30) std::cout << ", ";
   }
   std::cout << "\n";
+#endif
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "minimal <tflite model>\n");
+    return 1;
+  }
+
+  auto model_filename = argv[1];
+
+  // Load model
+  std::unique_ptr<tflite::FlatBufferModel> model =
+      tflite::FlatBufferModel::BuildFromFile(model_filename);
+  TFLITE_DEEPLAB_CHECK(model != nullptr);
+
+  // Build the interpreter
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+  InterpreterBuilder builder(*model, resolver);
+  std::unique_ptr<Interpreter> interpreter;
+  builder(&interpreter);
+  TFLITE_DEEPLAB_CHECK(interpreter != nullptr);
+
+  // Allocate tensor buffers.
+  TFLITE_DEEPLAB_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
+
+  std::vector<uint64_t> tp_acc, fp_acc, fn_acc;
+
+  for (int i = 1; i <= 2000; i++) {
+    string ground_truth_prefix = "/tmp/ade20k_512/annotations/raw/ADE_val_0000";
+    string input_bitmap_prefix = "/tmp/ade20k_512/images/bmp/ADE_val_0000";
+
+    char foo[5];
+    foo[4] = '\0';
+    std::snprintf(foo, 5, "%04d", i);
+    auto ground_truth_file = ground_truth_prefix + foo + ".raw";
+    auto input_bitmap_file = input_bitmap_prefix + foo + ".bmp";
+
+    std::vector<uint64_t> true_positives, false_positives, false_negatives;
+
+    run_model(interpreter, ground_truth_file, input_bitmap_file, true_positives,
+              false_positives, false_negatives);
+
+    if (i == 1) {
+      for (int j = 0; j < 31; j++) {
+        tp_acc.push_back(true_positives[j]);
+        fp_acc.push_back(false_positives[j]);
+        fn_acc.push_back(false_negatives[j]);
+      }
+    } else {
+      for (int j = 0; j < 31; j++) {
+        tp_acc[j] += true_positives[j];
+        fp_acc[j] += false_positives[j];
+        fn_acc[j] += false_negatives[j];
+      }
+    }
+
+    std::cout << i << "\n";
+#if __DEBUG__
+    std::cout << i << ": ";
+    for (int j = 0; j < 31; j++) {
+      std::cout << tp_acc[j] << ", " << fp_acc[j] << ", " << fn_acc[j];
+      if (j < 30) std::cout << ", ";
+    }
+    std::cout << "\n";
+#endif
+  }
+
+  uint64_t tp_sum = 0, fp_sum = 0, fn_sum = 0;
+
+  for (int j = 0; j < 31; j++) {
+    std::cout << "mIOU class " << j + 1 << ": "
+              << tp_acc[j] * 1.0 / (tp_acc[j] + fp_acc[j] + fn_acc[j]) << "\n";
+
+    tp_sum += tp_acc[j];
+    fp_sum += fp_acc[j];
+    fn_sum += fn_acc[j];
+  }
+
+  std::cout << "mIOU over_all: " << tp_sum << ", " << fp_sum << ", " << fn_sum
+            << ", " << tp_sum * 1.0 / (tp_sum + fp_sum + fn_sum) << "\n";
 
   return 0;
 }
